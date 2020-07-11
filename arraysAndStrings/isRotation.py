978c52175a951e43a4837b2d81bd65c9b6501ecb
Justin Large <justinlarge1974@gmail.com>
2020-07-11 06:22:13 -0700

    Removed the exercises I did a year or so ago.  I'm going to to them again

--- arraysAndStrings/isRotation.py
+++
@@ -1,9 +0,0 @@
-def isRotation(s1, s2) :
-    if len(s1) != len(s2) : return False
-    s2Times2 = s2 + s2
-    return s1 in s2Times2
-
-assert(isRotation('waterbottle', 'erbottlewat'))
-assert(not isRotation('waterbottle', 'erboxxlewat'))
-assert(not isRotation('waterttle', 'erbottlewat'))
-print('Done')
